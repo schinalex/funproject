618839b3f2894bbc482e4bbf45878aea77e60863
Academy <academy@Academys-iMac.local>
2016-04-26 20:13:46 +0300

    trying to convert validation code from C to js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var mysql = require('mysql')
 
 app.get('/love', function (req, res) {
   res.send('What is love!?')
@@ -12,3 +13,16 @@ app.get('/', function (req, res) {
 app.listen(3000, '0.0.0.0', function () {
   console.log('Example app listening on port 3000!')
 })
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'me',
+  password: 'secret',
+  database: 'my_db'
+})
+connection.connect()
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err
+  console.log('The solution is: ', rows[0].solution)
+})
+connection.end()
