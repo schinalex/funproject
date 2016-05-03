d6fb8badfdcf10dac6e1aeda24090b03b3019583
Academy <academy@Academys-iMac.local>
2016-05-03 20:25:18 +0300

    server connects to the db

--- server.js
+++ server.js
@@ -16,13 +16,48 @@ app.listen(3000, '0.0.0.0', function () {
 
 var connection = mysql.createConnection({
   host: 'localhost',
-  user: 'me',
-  password: 'secret',
-  database: 'my_db'
+  user: 'root',
+  password: '21032000Iulia',
+  database: 'battleship'
 })
 connection.connect()
 connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
   if (err) throw err
   console.log('The solution is: ', rows[0].solution)
 })
+
+var addPlayer = function (name, secretKey) {
+  connection.query('INSERT INTO Players (name, secretKey) values (\'' + name + '\', \'' + secretKey + '\')', function (err, rows, fields) {
+    if (err) throw err
+    console.log(rows)
+  })
+}
+
+var listPlayers = function () {
+  connection.query('SELECT * from Players', function (err, rows, fields) {
+    if (err) throw err
+    console.log(rows)
+  })
+}
+
+var initiateKeys = function (nrKeys) {
+  for (var i = 0; i < nrKeys; i++) {
+    addPlayer('', randomPassword(10))
+  }
+}
+
+function randomPassword (length) {
+  var chars = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890'
+  var pass = ''
+  for (var x = 0; x < length; x++) {
+    var i = Math.floor(Math.random() * chars.length)
+    pass += chars.charAt(i)
+  }
+  return pass
+}
+
+initiateKeys(100)
+addPlayer('Eugen', 'askhdhalksjd')
+listPlayers()
+
 connection.end()
