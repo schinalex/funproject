04bad5f448587908282d5cb871112cac5637447c
Academy <academy@Academys-iMac-7.local>
2016-05-30 19:56:38 +0300

    wrote style for the table

--- src/js/table.js
+++ src/js/table.js
@@ -1,48 +1,19 @@
 const MapUpload = angular.module('MapUpload', ['ngMaterial'])
 
 MapUpload.controller('mainCtrl', ['$scope', '$log', '$http', function ($scope, $log, $http) {
-  var table = [
-    {
-      place: 1,
+  var table = []
+  var tableSize = 50
+  for (var i = 0; i < tableSize; i++) {
+    table.push({
+      place: i + 1,
       player: 'John',
       mapsPlayed: 5,
       mapsLeft: 13,
       totalMisses: 450,
       averageMisses: 30
-    },
-    {
-      place: 1,
-      player: 'John',
-      mapsPlayed: 5,
-      mapsLeft: 13,
-      totalMisses: 450,
-      averageMisses: 30
-    },
-    {
-      place: 1,
-      player: 'John',
-      mapsPlayed: 5,
-      mapsLeft: 13,
-      totalMisses: 450,
-      averageMisses: 30
-    },
-    {
-      place: 1,
-      player: 'John',
-      mapsPlayed: 5,
-      mapsLeft: 13,
-      totalMisses: 450,
-      averageMisses: 30
-    },
-    {
-      place: 1,
-      player: 'John',
-      mapsPlayed: 5,
-      mapsLeft: 13,
-      totalMisses: 450,
-      averageMisses: 30
-    }
-  ]
+    })
+  }
+
   $scope.table = table
   const success = (res) => {
     $log.log(res.data)
@@ -52,4 +23,5 @@ MapUpload.controller('mainCtrl', ['$scope', '$log', '$http', function ($scope, $
   $scope.getResults = () => {
     $http.get('/getResults').then(success, error)
   }
+  console.log(table.length)
 }])
